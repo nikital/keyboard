c78e73bc1c8fb8866074fdc233a4e1121c35cf0a
Nikita Leshenko <nikita@leshenko.net>
2022-01-08 23:10:28 +0200

    fw: clangd config

---
+++ fw/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
+  . ((lsp-clients-clangd-args
+      . ("-query-driver=/usr/bin/avr-gcc")))))
